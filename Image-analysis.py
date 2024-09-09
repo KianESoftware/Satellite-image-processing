30077eb0bcbf6c5f9d618a3f835bf63eebff6459
KianEP <110359379+KianESoftware@users.noreply.github.com>
2024-09-09 08:31:09 +0300

    Update Image-analysis.py

--- Image-analysis.py
+++ Image-analysis.py
@@ -1,17 +1,4 @@
-```
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import seaborn
-import matlab.engine
-```
-eng = matlab.engine.start_matlab()
 
-image_path = 'path/to/image/'
-
-image = cv2.imread(image_path)
-
-image_gray = cv2.imread(image_path,0)
 
 
 
