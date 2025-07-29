0339e9e0c848ce84409d3ee9b6bfe5bd65b6d85f
kianep <110359379+KianESoftware@users.noreply.github.com>
2025-07-30 00:37:06 +0300

    Update .py

--- .py
+++ .py
@@ -8,8 +8,8 @@ import matplotlib
 config = SHConfig()
 config.sh_client_id = "******************"
 config.sh_client_secret = "********************"
-config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
-config.sh_base_url = "https://sh.dataspace.copernicus.eu"
+config.sh_token_url = "****************************************************8"
+config.sh_base_url = "************************************8"
 
 resolution = 10
 
